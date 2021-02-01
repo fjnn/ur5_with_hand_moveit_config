ce3b6f53f6c8905dc7002e66e8b352bb3d0d8c2d
gizem <gizemates@iyte.edu.tr>
2021-02-01 15:20:23 +0100

    hand series of actions works

--- src/hand_control_with_leap.py
+++ src/hand_control_with_leap.py
@@ -6,38 +6,33 @@ import moveit_commander
 import geometry_msgs.msg
 
 
-moveit_commander.roscpp_initialize(sys.argv)
-rospy.init_node("hand_control_with_leap_node", anonymous=False)
-robot = moveit_commander.RobotCommander()
-
-hand_group = moveit_commander.MoveGroupCommander("hand")
-hand_group.set_named_target("handOpen")
-plan1 = hand_group.go()
-
-rospy.sleep(5)
-moveit_commander.roscpp_shutdown()
 
 
-#import rospy
-#from std_msgs.msg import String
-#
-#def talker():
-#    pub = rospy.Publisher('chatter', String, queue_size=10)
-#    rospy.init_node('talker', anonymous=True)
-#    rate = rospy.Rate(10) # 10hz
-#    while not rospy.is_shutdown():
-#        hello_str = "hello world %s" % rospy.get_time()
-#        rospy.loginfo(hello_str)
-#        pub.publish(hello_str)
-#        rate.sleep()
-#
-#if __name__ == '__main__':
-#    try:
-#        talker()
-#    except rospy.ROSInterruptException:
-#        pass
 
 
+
+
+def main():
+    try:
+        moveit_commander.roscpp_initialize(sys.argv)
+        rospy.init_node("hand_control_with_leap_node", anonymous=False)
+        robot = moveit_commander.RobotCommander()
+        
+        hand_group = moveit_commander.MoveGroupCommander("hand")
+        hand_group.set_named_target("handClosed")
+        plan = hand_group.go()        
+#        rospy.sleep(5)
+        hand_group.set_named_target("handOpen")
+        plan = hand_group.go()        
+        rospy.sleep(5)
+
+    except KeyboardInterrupt:
+        moveit_commander.roscpp_shutdown()
+        rospy.signal_shutdown("KeyboardInterrupt")
+        raise
+
+
+if __name__ == '__main__': main()
 
 
 
