a9dcb582e975f69ca3539f1a16cf1dd042247ec7
gizem <gizemates@iyte.edu.tr>
2021-02-02 15:17:00 +0100

    kwargs argments taken

--- src/hand_control_with_leap.py
+++ src/hand_control_with_leap.py
@@ -3,6 +3,7 @@
 """
 It looks like I will use move_groups for EE mapping and action server for joint space mapping
 TODO: This whole thing can be written in classes. My_Move_Groups(). Don't overcode yet :D
+Refer to: http://docs.ros.org/en/indigo/api/pr2_moveit_tutorials/html/planning/scripts/doc/move_group_python_interface_tutorial.html
 """
 
 import sys
@@ -23,6 +24,10 @@ from Classes.leap_class import LeapSubscriber
 
 
 def movegroup_init():
+    """
+    Initializes the manipulator and end-effector groups
+    @returns Initialized groups
+    """
     moveit_commander.roscpp_initialize(sys.argv)
 #    rospy.init_node("hand_control_with_leap_node", anonymous=False)
     robot = moveit_commander.RobotCommander()
@@ -36,7 +41,7 @@ def movegroup_init():
     return hand_group, arm_group
     
 
-def task_space_map():
+def task_space_control():
     """
     Here I will use predefined poses for robot to move.
     Move group will be used like that:
@@ -58,6 +63,48 @@ def rt_ee_mapping():
     pass
 
 
+def joint_names_to_numbers(argument): 
+    switcher = { 
+        "shoulder_pan": 0, 
+        "shoulder_lift": 1,
+        "wrist_2": 4,
+    } 
+  
+    # get() method of dictionary data type returns  
+    # value of passed argument if it is present  
+    # in dictionary otherwise second argument will 
+    # be assigned as default value of passed argument 
+    return switcher.get(argument, "nothing") 
+
+
+def joint_space_control(hand_group, arm_group, **kwargs):
+    """
+    IMU readings will be mapped real time.
+    @param hand_group: end-effector group name
+    @param arm_group: manipulator group name
+    @param **kwargs: [joint_x, value] list
+    """
+    arm_group.clear_pose_targets()
+    hand_group.clear_pose_targets()
+    
+    arm_group_variable_values = arm_group.get_current_joint_values()
+    print "============ Arm joint values: ", arm_group_variable_values
+    hand_group_variable_values = hand_group.get_current_joint_values()
+    print "============ Hand joint values: ", hand_group_variable_values
+    print "click Enter to continue"
+    dummy_input = raw_input()
+#    arm_group_variable_values[4] = 1.0
+#    arm_group.set_joint_value_target(arm_group_variable_values)
+    for joint,value in kwargs.items():
+        joint_int = joint_names_to_numbers(joint)
+        joint_val = value
+    print joint_int, joint_val
+    print "click Enter to continue"
+    dummy_input = raw_input()
+    arm_group_variable_values[joint_int] = joint_val
+    arm_group.set_joint_value_target(arm_group_variable_values)
+    plan_arm = arm_group.go() 
+    
 def rt_joints_mapping(hand_group, arm_group):
     """
     IMU readings will be mapped real time.
@@ -79,14 +126,14 @@ def rt_joints_mapping(hand_group, arm_group):
     plan_arm = arm_group.go()  
     
     dummy_input = raw_input()
-    
-
+        
 
 def main():
     try:
         hand_group, arm_group = movegroup_init()
 #        rospy.sleep(5)
-        rt_joints_mapping(hand_group, arm_group)
+        joint_space_control(hand_group, arm_group, wrist_2=1.0)
+#        rt_joints_mapping(hand_group, arm_group)
         sys.exit("done")
 #        IMU.init_subscribers_and_publishers()
 #        
